aa7c145fb4a4947e946f295ee082241aaa03d445
hazelgithub <35742897+hazelgithub@users.noreply.github.com>
2018-05-26 20:02:45 -0400

    added python 1 file

---
+++ PGM1.py
@@ -0,0 +1,20 @@
+print("Hello World")
+print("Manish")
+a = [1,2,3,4,5]
+print ("I am Learning Python")
+print ("What about you ???")
+print ("python'd is good")
+print ("I like typing this. ") # Manish
+#print ('I like to "code"')
+##
+print ("Manish is ", 1, "Roll")
+for i in a :
+    print (i)
+    print ( "in loop" )
+print("end of loop")
+b = {}
+b = {1,4,6,3,4,4,4,6}
+print(b)
+for j in b:
+    print (j+2.5)
+##
